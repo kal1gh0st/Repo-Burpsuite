0537d32474fe8dbe0928c825215ca678c8712fbf
Francesco Fedele <56889513+kal1gh0st@users.noreply.github.com>
2021-06-02 10:52:51 +0200

    Delete ssl-scan directory

--- ssl-scan/lib/ssl_scan/util.rb
+++
@@ -1,7 +0,0 @@
-module SSLScan
-  module Util
-
-    ROOT = File.expand_path("../../../", __FILE__)
-
-  end
-end
--- ssl-scan/lib/ssl_scan/version.rb
+++
@@ -1,9 +0,0 @@
-module SSLScan
-  module VERSION
-    MAJOR = 0
-    MINOR = 0
-    PATCH = 6
-
-    STRING = [MAJOR,MINOR,PATCH].join('.')
-  end
-end
